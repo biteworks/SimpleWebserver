82c72f8fd5548e9673585cca1cd8c66ae5db6b17
Biteworks <twitter@tobiaswilhelm.info>
2023-03-13 17:14:38 +0100

    Initial setup

---
+++ SimpleWebserver/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using static SimpleWebserver.HTTPServer;
+
+namespace SimpleWebserver
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string desktopPath = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+            string pathToWebsite = desktopPath + "\\Website";
+            string ip = "localhost";
+            int port = 8080;
+
+            HTTPServer server = new HTTPServer(pathToWebsite, ip, port);
+            server.Start();
+            Console.ReadKey();
+        }
+    }
+}
